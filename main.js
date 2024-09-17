238972aad027091ed9524f5d15c3f287117f5223
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-18 00:28:34 +0600

    modification added and description added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById("delete-input").addEventListener("keyup", function(event){
-    // console.log(event.target.value);
+    // event.target.value  ---->  get the value from the text-box while user typing (using keyup event) 
+
     if(event.target.value === "delete"){
         document.getElementById("btn-delete").removeAttribute("disabled");
     }
@@ -11,6 +12,14 @@ document.getElementById("delete-input").addEventListener("keyup", function(event
 
 document.getElementById("btn-delete").addEventListener("click", function(){
    
-    document.getElementById("delete-text").style.display = "none";
+    // hiding or removing the text
+    //document.getElementById("delete-text").style.display = "none";  // option-1
+    // or
+    document.getElementById("delete-text").setAttribute("hidden", true); // option-2
+
+    // make the button disabled again
+    document.getElementById("btn-delete").setAttribute("disabled", true);
+
+    // clear the input text field
     document.getElementById("delete-input").value = "";
 });
